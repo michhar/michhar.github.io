4ce491634b8bbe680666fab5886aea802b8fe242
michhar <michhar@microsoft.com>
2017-12-11 23:43:13 -0800

    image and scripts update

--- bin/scale.py
+++ bin/scale.py
@@ -1,10 +1,11 @@
 import cv2
 import glob
 
-files = glob.glob("**/*.png", recursive=True)
+files = glob.glob("*.jpg", recursive=False)
 
 for f in files:
     try:
+        print(f)
         im = cv2.imread(f)
         im = cv2.resize(im, (675, 450))
         cv2.imwrite(f, im)
